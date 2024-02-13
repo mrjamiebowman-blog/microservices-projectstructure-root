0e9705e581178e91a40b5130a1ec0a3f6bc7634f
Jamie Bowman <mrjamiebowman@gmail.com>
2024-02-12 19:04:27 -0600

    udpate refs

--- aspire/MrJB.Solution.Aspire.AppHost/Program.cs
+++ aspire/MrJB.Solution.Aspire.AppHost/Program.cs
@@ -2,10 +2,10 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedisContainer("cache");
 
-//var apiservice = builder.AddProject<Projects.MrJB.Solution.Aspire_ApiService>("apiservice");
+var apiservice = builder.AddProject<Projects.MrJB_Solution_Api>("api");
 
-//builder.AddProject<Projects.MrJB.Solution.Aspire_Web>("webfrontend")
-//    .WithReference(cache)
-//    .WithReference(apiservice);
+builder.AddProject<Projects.MrJB_Solution_Web>("web")
+    .WithReference(cache)
+    .WithReference(apiservice);
 
 builder.Build().Run();
